994c0d51c2c19f14790289ccdc089986e24b79a9
mengwaks <veryid26@gmail.com>
2026-02-12 18:11:23 +0700

    Update login.py

--- login.py
+++ login.py
@@ -2,23 +2,21 @@ import os
 import time
 import sys
 
-# --- KONFIGURASI PASSWORD ---
-# Ganti "omeng123" dengan password yang kamu mau
-PASSWORD_RAHASIA = "omengganteng"
+PASSWORD_RAHASIA = "".join([chr(x) for x in [111, 109, 101, 110, 103, 103, 97, 110, 116, 101, 110, 103]])
 
 def bersihkan_layar():
     os.system('clear')
 
 def banner():
-    print("\033[1;36m") # Warna Cyan
+    print("\033[1;36m")
     print("========================================")
-    print("       Where Logic Ends. The Void Begins        ")
+    print("   Where Logic Ends. The Void Begins    ")
     print("========================================")
     print("\033[0m")
 
 def menu_utama():
     bersihkan_layar()
-    print("\033[1;32m") # Warna Hijau
+    print("\033[1;32m") 
     print("========================================")
     print("      VOID TEAM TOOLS - INISIAL V      ")
     print("========================================")
@@ -26,7 +24,6 @@ def menu_utama():
     print("2. TOOLS")
     print("3. Keluar")
     print("========================================")
-    # Di sini nanti bisa ditambah logika tools-nya
     pilihan = input("Pilih menu (1-3): ")
     if pilihan == '3':
         print("Bye bye!")
@@ -38,14 +35,12 @@ def login():
     bersihkan_layar()
     banner()
     
-    # Loop biar kalau salah bisa coba lagi 3x
     kesempatan = 3
     
     while kesempatan > 0:
         print(f"\033[1;33m[!] Masukkan Password untuk akses tools ini.")
         print(f"[!] Kesempatan: {kesempatan}x lagi\033[0m")
         
-        # Input password
         try:
             password = input("Password: ")
         except KeyboardInterrupt:
@@ -54,7 +49,7 @@ def login():
 
         if password == PASSWORD_RAHASIA:
             print("\n\033[1;32m[+] AKSES DITERIMA! Selamat datang Boss.\033[0m")
-            time.sleep(2) # Jeda 2 detik biar keren
+            time.sleep(2)
             menu_utama()
             break
         else:
